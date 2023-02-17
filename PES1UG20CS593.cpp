264d485cdb6bdfb7ddca0000fdadc1125383d343
Sinchanad258 <115353454+Sinchanad258@users.noreply.github.com>
2023-02-17 09:28:39 +0530

    Update PES1UG20CS593.cpp

--- PES1UG20CS593.cpp
+++ PES1UG20CS593.cpp
@@ -5,6 +5,6 @@ int main(void) {
   cout << "PES1UG20CS593\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n
+  cout << "I have successfully built and run\n";
   return 0;
 }
